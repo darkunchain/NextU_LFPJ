8f196c4703b2adce5f5bf55ae1464cf862d6dc2f
darkunchain <luisfer74081@gmail.com>
2019-03-18 16:09:39 -0500

    server

---
+++ Buscador_NodeJs/src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+// inicializacion
+const app = express();
+
+//importar rutas
+const rutas = require('./routes/index');
+
+// configuracion
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+// archivos estaticos
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', rutas);
+
+// inicio del servidor
+app.listen(app.get('port'), () => {
+  console.log('Servidor corriendo en el puerto ', app.get('port'));
+});
