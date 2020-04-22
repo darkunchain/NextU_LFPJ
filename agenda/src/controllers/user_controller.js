76e82374685fb4e9885c0b794e08e177f906d548
darkunchain <luisfer74081@gmail.com>
2020-04-21 20:27:12 -0500

    hoy

--- agenda/src/controllers/user_controller.js
+++ agenda/src/controllers/user_controller.js
@@ -12,12 +12,17 @@ users.signup = async(req, res) => {
     res.render('signup')    
 }
 
-users.newUser = async(req, res) => {    
+users.newUser = async(req, res) => {
+    console.log('signup_reqbody: ', req) 
     res.send('nuevo usuario')    
 }
 
 users.authUser = async(req, res) => {    
     res.send('validar usuario')    
+}
+
+users.logout = async(req, res) => {    
+    res.send('logout')    
 }
 
 module.exports = users
